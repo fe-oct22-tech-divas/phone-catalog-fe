22ebeffa8653da22ef05d0ed4b0e01807b89905f
Diana Demkiv <dianademkiv789@gmail.com>
2023-02-28 22:57:07 +0200

    added react-router and phones page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './App.scss';
 import { Header } from './modules/shared/Header';
 
 export const App: React.FC = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/phones">Phones</Link>
+      </nav>
+      <div className="starter">
+      </div>
+    </>
   );
 };
