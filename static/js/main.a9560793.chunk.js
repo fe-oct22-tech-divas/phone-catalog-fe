a2ecc84f858b4edf35fc94efbab2bf4791ec7fb2
Fynkzia <Fynkzia@users.noreply.github.com>
2023-03-01 11:52:19 +0000

    deploy: 933edf288965e3acd596029a536bf103c1418467

--- static/js/main.a9560793.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpphone-catalog-fe"]=this["webpackJsonpphone-catalog-fe"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var c=n(0),o=n.n(c),s=n(3),a=n.n(s),r=(n(8),n(9),n(1)),i=function(){return Object(r.jsx)("div",{className:"starter"})};a.a.render(Object(r.jsx)(o.a.StrictMode,{children:Object(r.jsx)(i,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.a9560793.chunk.js.map
