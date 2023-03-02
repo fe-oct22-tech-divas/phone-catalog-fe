f60309c175db51ea79c2762ee11ecff1fc3a7586
Olena Radchuk <allenradchuk@gmail.com>
2023-03-02 22:36:01 +0200

    created footer for desktop

--- src/modules/shared/Footer/Footer.tsx
+++ src/modules/shared/Footer/Footer.tsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.scss';
 
-export const Footer: React.FC = () => {
+export const Footer = () => {
+  const toTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <h1>HomePage</h1>
+    <footer className="footer">
+      <div className="footer__container">
+        <div className="footer__content">
+          <a href="/home">
+            <div className="footer__logo" />
+          </a>
+          <nav className="footer__nav">
+            <ul className="footer__list">
+              <li className="footer__item">
+                <a href="/github" className="footer__link">GITHUB</a>
+              </li>
+              <li className="footer__item">
+                <Link
+                  to="/contacts"
+                  className="footer__link"
+                >
+                  CONTACTS
+                </Link>
+              </li>
+              <li className="footer__item">
+                <Link
+                  to="/rights"
+                  className="footer__link"
+                >
+                  RIGHTS
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <div className="footer__top">
+            <span className="footer__top-title">Back to top</span>
+            <button
+              type="button"
+              className="footer__top-button"
+              aria-label="top-button"
+              onClick={toTop}
+            />
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 };
